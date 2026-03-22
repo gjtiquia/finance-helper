4b051c5ca76adc10f58171d22cc928f6d3bdc1d5
GJ Tiquia <gjtiquia@gmail.com>
2026-03-22 13:35:11 +0800

    added newlines

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -8,9 +8,11 @@ import (
 func main() {
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
+
 		case "--help", "-h":
 			fmt.Println("TODO : help page")
 			return
+
 		case "connect":
 			if len(os.Args) != 3 {
 				fmt.Println("Usage: finance-helper connect <url>")
@@ -23,6 +25,7 @@ func main() {
 			}
 
 			return
+
 		case "status":
 			if err := status(os.Stdout); err != nil {
 				fmt.Println(err.Error())
